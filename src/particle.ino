b8e40fa225dde10b66c3e881e43f0fdea2fece95
mo8ammed-saleh <mo7d_obi@hotmail.com>
2021-12-12 11:43:11 +0400

    Hi Madona, I add a comment

--- src/particle.ino
+++ src/particle.ino
@@ -6,7 +6,7 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   for (int i=1 ; i<= 10; i++){
-      Particle.publish ("Print", String (i));
+      Particle.publish ("Print", String (i)); //Code thst publish values
       delay(1000);
       
   }
